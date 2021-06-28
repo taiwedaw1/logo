2b47bac5bd68ca3f8f18803d0840a905690179b0
taiwedaw1 <86561508+taiwedaw1@users.noreply.github.com>
2021-06-28 09:56:44 +0800

    s
    s

---
+++ config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  Admins: ["784719972890574869", "702705968202645526"], //Admins of the bot
+  ExpressServer: true,//If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || "?", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/xaHxTeSNa3", //Support Server Link
+  Token: process.env.Token || "ODUyNzk1NzMwNzY1NTQ1NDky.YMMB6g.cnxVqTkMBRegpA-t07FGBR45TVU", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "852795730765545492", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "vb-OmsbKW1sdCazbK_P60MNI5yPIeKee", //Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
+  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: "Pikachu is cute", //A Secret like a password
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  Permissions: 2205280576, //Bot Inviting Permissions
+  Website: process.env.Website || "https://Dahsboard.taiwedaw.repl.co", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+
+  //Lavalink
+   Lavalink: {
+    id: "Main",
+    host: "lava.link",
+    port: 80,
+    pass: "youshallnotpass", 
+  },
+  
+  //Alternate Lavalink
+  /*
+  Lavalink: {
+    id: "Main",
+    host: "lava.sudhan.tech",
+    port: 1234,
+    pass: "CodingWithSudhan", 
+  },
+  */
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || " d446ad582a4143608e40114df74da87b", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "c6fdc1e6904c42e38839312fbba8d574", //Spotify Client Secret
+  },
+};
